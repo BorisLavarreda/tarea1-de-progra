141e76052a16ae8bf2c37aea6838150aafc2ad5a
BorisLavarreda <alelavarreda.asael2021@gmail.com>
2026-03-26 11:32:24 -0600

    Tarea1 programacion

---
+++ Sentencias logicas.py
@@ -0,0 +1,23 @@
+nombre = "Boris"
+if nombre == "Boris":
+    print("Boris Esta parado")
+
+nombre = "Nelvyn"
+if nombre == "Nelvyn":
+    print("Nelvyn Esta sentado")
+
+nombre = "Cristiano"
+if nombre == "Cristiano":
+    print("Cristiano Esta bailando")
+
+nombre = "Pessi"
+if nombre == "Pessi":
+    print("pessi Esta sentado")
+
+nombre = "Isa"
+if nombre == "Isa":
+    print("esta sentada")
+
+nombre = "Cabezas"
+if nombre == "Cabezas":
+    print("Cabezas es hermoso")
